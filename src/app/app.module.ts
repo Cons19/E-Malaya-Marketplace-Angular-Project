067111cc8bedc01a5a752e8bb6a30e7a2877403f
Paul Panaitescu <paul.panaitescu.dk@gmail.com>
2019-05-16 13:50:23 +0200

    components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { DisplayProductsComponent } from './display-products/display-products.component';
+import { DisplayProductComponent } from './display-product/display-product.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    DisplayProductsComponent,
+    DisplayProductComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/display-product/display-product.component.spec.ts
@@ -0,0 +1,25 @@
+import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { DisplayProductComponent } from './display-product.component';
+
+describe('DisplayProductComponent', () => {
+  let component: DisplayProductComponent;
+  let fixture: ComponentFixture<DisplayProductComponent>;
+
+  beforeEach(async(() => {
+    TestBed.configureTestingModule({
+      declarations: [ DisplayProductComponent ]
+    })
+    .compileComponents();
+  }));
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(DisplayProductComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
