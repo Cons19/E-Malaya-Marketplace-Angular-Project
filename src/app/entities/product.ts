755665cd4ca046e53e863fe1bfe07405e5ad5045
Paul Panaitescu <paul.panaitescu.dk@gmail.com>
2019-05-16 14:35:29 +0200

    user, product entities

---
+++ src/app/entities/product.ts
@@ -0,0 +1,19 @@
+import { User} from './user';
+
+export class Product {
+    _id: string;
+    name: string;
+    user: User;
+    description: string;
+    price: Price;
+}
+
+export class Price {
+    value: number;
+    currency: string;
+}
+
+export enum Type {
+    Electronics, Fashion, Furniture, Toys
+}
+
---
+++ src/app/entities/user.ts
@@ -0,0 +1,16 @@
+
+export class User {
+    _id: string;
+    firstName: string;
+    lastName: string;
+    email: string;
+    password: string;
+    gender?: Gender; 
+    birthDate?: Date;
+    phone?: number;
+    address?: string;
+}
+
+export enum Gender {
+    Male, Female, Unspecified
+}
