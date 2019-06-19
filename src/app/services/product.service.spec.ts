6692dcd53fb8cf26c3253ac3c9e931461ef3c7d7
marius <dmunteanu91@yahoo.com>
2019-06-19 14:35:54 +0200

    unit test

--- src/app/services/product.service.spec.ts
+++ src/app/services/product.service.spec.ts
@@ -1,12 +1,48 @@
-// import { TestBed } from '@angular/core/testing';
+import {TestBed} from '@angular/core/testing';
 
-// import { ProductService } from './temp-data.service';
+import {ProductService} from './product.service';
+import {Product} from '../entities/product';
 
-// describe('ProductService', () => {
-//   beforeEach(() => TestBed.configureTestingModule({}));
+describe('ProductService', () => {
+  it('should get all products', function() {
+    const service: ProductService = TestBed.get(ProductService);
+    const products: Product[] = service.getProducts();
 
-//   it('should be created', () => {
-//     const service: ProductService = TestBed.get(ProductService);
-//     expect(service).toBeTruthy();
-//   });
-// });
+    expect(products).not.toBeNull();
+  });
+
+  it('should get a product', function() {
+    const service: ProductService = TestBed.get(ProductService);
+    const products: Product[] = service.getProducts();
+
+    expect(service.getProduct(products[0]._id)).toBe(products[0]);
+  });
+
+  it('should add a product', function() {
+    const service: ProductService = TestBed.get(ProductService);
+    const product: Product = {
+      _id: '999',
+      name: 'test product',
+      description: 'test description',
+      price: {
+        value: 99.99,
+        currency: 'DKK'
+      }
+    };
+
+    service.addProduct(product);
+
+    expect(service.getProduct(product._id)).toBe(product);
+  });
+
+  it('should update a product', function() {
+    const service: ProductService = TestBed.get(ProductService);
+
+    const product: Product = service.getProducts()[0];
+    product.name = "test product modified";
+
+    service.updateProduct(product);
+
+    expect(service.getProducts()[0]).toBe(product);
+  });
+});
