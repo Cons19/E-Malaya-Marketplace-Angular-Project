e8dc5406f64ca7b85cc52e6dc8ab0c2443136e94
marius <dmunteanu91@yahoo.com>
2019-06-19 14:53:06 +0200

    unit test

--- src/app/services/cart.service.spec.ts
+++ src/app/services/cart.service.spec.ts
@@ -1,12 +1,45 @@
-// import { TestBed } from '@angular/core/testing';
+import {TestBed} from '@angular/core/testing';
 
-// import { CartService } from './cart.service';
+import {CartService} from './cart.service';
+import {FullCartItem} from '../entities/cart';
+import {ProductService} from './product.service';
+import {Product} from '../entities/product';
 
-// describe('CartService', () => {
-//   beforeEach(() => TestBed.configureTestingModule({}));
+describe('CartService', () => {
+  beforeEach(() => TestBed.configureTestingModule({}));
 
-//   it('should be created', () => {
-//     const service: CartService = TestBed.get(CartService);
-//     expect(service).toBeTruthy();
-//   });
-// });
+  it('should get all cart items', () => {
+    const service: CartService = TestBed.get(CartService);
+    const fullCartItems: FullCartItem[] = service.getContents();
+
+    expect(fullCartItems).not.toBeNull();
+  });
+
+  it('should add a product', function() {
+    const service: CartService = TestBed.get(CartService);
+    const product: Product = TestBed.get(ProductService).getProducts()[0];
+
+    service.addProduct(product._id);
+
+
+    let content: FullCartItem = service.getContents().find(thatProduct => thatProduct.product === product);
+    expect(content.product).toBe(product);
+  });
+
+  it('should remove a product', function() {
+    const service: CartService = TestBed.get(CartService);
+    const product: Product = TestBed.get(ProductService).getProducts()[0];
+
+    service.addProduct(product._id);
+
+
+    let content: FullCartItem = service.getContents().find(thatProduct => thatProduct.product === product);
+    expect(content.product).toBe(product);
+
+    service.removeProduct(product._id);
+
+    content = service.getContents().find(thatProduct => thatProduct.product === product);
+
+    expect(content).toBeUndefined();
+  });
+});
