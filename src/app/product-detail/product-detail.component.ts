35ca3a96044ca2708c6a22898bf031544e271a85
MariusMM2 <dmunteanu91@yahoo.com>
2019-06-21 17:11:01 +0200

    added isAdmin check

--- src/app/product-detail/product-detail.component.ts
+++ src/app/product-detail/product-detail.component.ts
@@ -4,6 +4,8 @@ import {ProductService} from '../services/product.service';
 import {ActivatedRoute} from '@angular/router';
 import {CartService} from '../services/cart.service';
 import {Observable} from 'rxjs';
+import {NgRedux} from "@angular-redux/store";
+import {AppState} from "../store";
 
 @Component({
   selector: 'app-product-detail',
@@ -13,15 +15,17 @@ import {Observable} from 'rxjs';
 export class ProductDetailComponent implements OnInit {
 
   product: Observable<Product>;
+  isAdmin$: Observable<boolean>;
 
-  constructor(private productService: ProductService, private route: ActivatedRoute, private cartService: CartService) { }
-
-  ngOnInit() {
-
-    this.retrieveProduct();
+  constructor(private ngRedux: NgRedux<AppState>, private productService: ProductService, private route: ActivatedRoute, private cartService: CartService) {
   }
 
-  async retrieveProduct() {
+  ngOnInit() {
+    this.retrieveProducts();
+    this.isAdmin$ = this.ngRedux.select(state => state.isAdmin);
+  }
+
+  async retrieveProducts() {
     // Get the id from the url
     const id = this.route.snapshot.paramMap.get('id');
     let products = <Observable<Product>>await this.productService.getProduct(id);
