cd52ab47f23619b26cae4f2fb3d5974a8705b384
MariusMM2 <dmunteanu91@yahoo.com>
2019-06-23 11:49:44 +0200

    added loading widget

--- src/app/product-list/product-list.component.ts
+++ src/app/product-list/product-list.component.ts
@@ -14,14 +14,22 @@ import {Product} from '../entities/product';
 export class ProductListComponent implements OnInit {
 
   products$: Observable<Product[]>;
-  isLoading: boolean;
+  isLoading$: Observable<boolean>;
   userSearch: string;
   isAdmin$: Observable<boolean>;
 
   constructor(private ngRedux: NgRedux<AppState>, private productActions: ProductActions, private productService: ProductService) {
   }
   ngOnInit() {
-    this.products$ = this.productService.getProducts();
+    this.isLoading$ = new Observable(subscriber => {
+      subscriber.next(true);
+
+      this.products$ = this.productService.getProducts();
+
+      setTimeout(()=>{
+        subscriber.next(false);
+      }, 2000)
+    });
     // create an observable for the isAdmin field
     this.isAdmin$ = this.ngRedux.select(state => state.products.isAdmin);
   }
