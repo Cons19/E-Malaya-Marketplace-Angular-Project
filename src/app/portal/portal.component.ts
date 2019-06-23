e74f04d496f808865c692b112bd2e7531d63e529
MariusMM2 <dmunteanu91@yahoo.com>
2019-06-23 12:39:54 +0200

    added text to show login type

--- src/app/portal/portal.component.ts
+++ src/app/portal/portal.component.ts
@@ -2,6 +2,9 @@ import {Component, OnInit} from '@angular/core';
 import {ProductActions} from '../product.actions';
 import {AuthService} from "../auth/auth.service";
 import {AdminService} from "../admin/admin.service";
+import {NgRedux} from "@angular-redux/store";
+import {AppState} from "../store";
+import {Observable} from "rxjs";
 
 @Component({
   selector: 'app-portal',
@@ -9,8 +12,10 @@ import {AdminService} from "../admin/admin.service";
   styleUrls: ['./portal.component.scss']
 })
 export class PortalComponent implements OnInit {
+  isAdmin$: Observable<boolean>;
 
-  constructor(private productActions: ProductActions, private authService: AuthService, private adminService: AdminService) {
+  constructor(private productActions: ProductActions, private authService: AuthService, private adminService: AdminService, private ngRedux: NgRedux<AppState>) {
+    this.isAdmin$ = this.ngRedux.select(state => state.products.isAdmin);
   }
 
   ngOnInit() {
