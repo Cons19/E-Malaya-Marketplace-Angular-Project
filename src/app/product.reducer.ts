8f019ee27a151866a1e6a7b93a2633a9c217474b
MariusMM2 <dmunteanu91@yahoo.com>
2019-06-24 08:46:47 +0200

    removed commented code

--- src/app/product.reducer.ts
+++ src/app/product.reducer.ts
@@ -9,23 +9,6 @@ const INITIAL_STATE: ProductState = {isLoggedIn: false, isAdmin: false
 
 export function productReducer(state: ProductState = INITIAL_STATE, action: any) {
   switch (action.type) {
-//   case ProductActions.GET_PRODUCTS_LOADING:
-//     return tassign(state, { isLoading: true });
-
-//   case ProductActions.GET_PRODUCTS_SUCCESS:
-//     return tassign(state, {isLoading: false, products: action.payload });
-
-//   case ProductActions.GET_PRODUCTS_FAILED:
-//     return tassign(state, {isLoading: false});
-
-    // case ProductActions.CREATE_PRODUCT:
-    //   return tassign(state, {products: [...state.products, action.payload]});
-
-//    case ProductActions.UPDATE_PRODUCT:
-//     return 
-
-//    case ProductActions.DELETE_PRODUCT:
-//     return 
     case ProductActions.LOG_IN:
       return tassign(state, {isLoggedIn: action.payload});
     case ProductActions.ADMIN:
